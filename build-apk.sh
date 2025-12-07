e260c8c6bdf26e3a0903229a18c723401a58ea43
sukriyajuharicat <sukriyajuhari@gmail.com>
2025-12-08 01:34:27 +0700

    Add final non-interactive Bubblewrap workflow

--- build-apk.sh
+++ build-apk.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Gunakan path JDK GitHub Actions
+# Path JDK di GitHub Actions
 export BUBBLEWRAP_JDK_PATH=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/17.0.17-10/x64
 
 # Build non-interaktif
 bubblewrap init --manifest=manifest.json --no-install --force
 bubblewrap build
-
